2d226e12fadd0ee4dbf6dd3ecb457a4bb2d15741
juncheng.ma <upgrader@qq.com>
2025-06-05 20:10:21 +0800

    feat: add test cses.

--- src/Aymadoka.Static.Extensions/DelegateExtension/Delegate.Combine.cs
+++ src/Aymadoka.Static.Extensions/DelegateExtension/Delegate.Combine.cs
@@ -4,6 +4,12 @@ namespace Aymadoka.Static.DelegateExtension
 {
     public static partial class DelegateExtensions
     {
+        /// <summary>
+        /// 合并两个委托实例，返回一个新的委托，表示这两个委托的组合
+        /// </summary>
+        /// <param name="a">第一个委托实例</param>
+        /// <param name="b">要组合的第二个委托实例</param>
+        /// <returns>合并后的委托实例</returns>
         public static Delegate Combine(this Delegate a, Delegate b)
         {
             return Delegate.Combine(a, b);
--- src/Aymadoka.Static.Extensions/DelegateExtension/Delegate.Remove.cs
+++ src/Aymadoka.Static.Extensions/DelegateExtension/Delegate.Remove.cs
@@ -4,6 +4,12 @@ namespace Aymadoka.Static.DelegateExtension
 {
     public static partial class DelegateExtensions
     {
+        /// <summary>
+        /// 从当前委托中移除与指定委托（value）相同的最后一个委托实例
+        /// </summary>
+        /// <param name="source">要从中移除委托的源委托</param>
+        /// <param name="value">要移除的委托实例</param>
+        /// <returns>移除指定委托后的新委托，如果未找到则返回原委托</returns>
         public static Delegate? Remove(this Delegate source, Delegate value)
         {
             return Delegate.Remove(source, value);
